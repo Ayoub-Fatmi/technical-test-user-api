3cdea08e3d571ac5a185ad544738acf604bef875
Ayoub-Fatmi <fatmiayoub04@gmail.com>
2026-01-17 20:29:49 +0100

    project initialization

---
+++ src/main/java/com/ayoub/technicaltestuserapi/TechnicalTestUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.ayoub.technicaltestuserapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestUserApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalTestUserApiApplication.class, args);
+    }
+
+}
